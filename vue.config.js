718d907345d57939994c93c78f6d44920e5b1a2f
lunnlew <lunnlew@qq.com>
2019-12-10 17:18:25 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -26,8 +26,7 @@ module.exports = {
           "target": [{
             "target": "nsis",
             "arch": [
-              // "ia32",
-              "x64"
+              "ia32"
             ]
           }],
           icon: './public/app.png'
