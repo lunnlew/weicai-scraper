f84bdd291cff2f43a2c83287dd77e613842f6147
lunnlew <lunnlew@qq.com>
2019-12-27 21:31:34 +0800

    特性(WeChatHelper): 增加对版本wechat-2.8.0.88,wechatwin.dll-2.8.0.88的支持

---
+++ src/native/WeChatHelper/WeChatHelper28088/dllmain.cpp
@@ -0,0 +1,33 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "stdafx.h"
+
+// 窗口消息循环
+#include "WndMsgLoop.h"
+
+
+//在使用Debug远程调试DLL必须要有__declspec函数 导出
+DLLAPI void test()
+{
+	OutputDebugStringA("__declspec(dllexport) test() \r\n");
+}
+
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH: {
+		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)InitWindow, hModule, 0, NULL);
+		break;
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
--- src/native/WeChatHelper/WeChatHelperLib/HookOffset.h
+++ src/native/WeChatHelper/WeChatHelperLib/HookOffset.h
@@ -82,4 +82,25 @@
 
 #define offset_WxAntiRevoke 0x0			//防撤回
 
+
+#elif WECHAT_WINDLL_VERSION == 28088
+
+#define offset_LoginSign 0x0	//登录标志位
+
+#define offset_ReciveMessage 0x350878-5		//接收消息
+#define offset_ReciveMessageParam 0x16288F8		//接收消息的参数
+#define offset_ReciveMessageParam_MsgType 0x30 //消息类型
+#define offset_ReciveMessageParam_MsgSourceType 0x34 //消息来源类型
+#define offset_ReciveMessageParam_MsgReciver 0x40 //消息的接收者
+#define offset_ReciveMessageParam_MsgContent 0x68 //消息主体
+#define offset_ReciveMessageParam_MsgSender 0x118 //消息的发送者
+
+#define offset_QrCode 0x0			//二维码
+#define offset_QrCodeContect 0x0	//二维码内容
+
+#define offset_GoToQrCode1 0x0			//跳转到二维码
+#define offset_GoToQrCode2 0x0			//跳转到二维码
+
+#define offset_WxAntiRevoke 0x28866B			//防撤回
+
 #endif
